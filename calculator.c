a7bee0be31fbac6b1c7ac5bc55aa7422768622b5
vriti22 <58466356+vriti22@users.noreply.github.com>
2019-12-06 00:27:08 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+printf("Simple Calculator\n");
+float a=10, b=2;
+float res;
+printf("Enter any two number\n");
+printf("Entered Number is: %f and %f\n",a,b);
+res=a+b;
+printf("sum of two no is: %f\n", res);
+res=a-b;
+printf("sub of two no is: %f\n", res);
+res=a*b;
+printf("mul of two no is: %f\n", res);
+res=a/b;
+printf("div of two no is: %f\n", res);
+
+return 0;
+}
